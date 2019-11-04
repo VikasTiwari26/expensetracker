0b5322d340094306478e959949f5ec566a71789a
VikasTiwari26 <54770104+VikasTiwari26@users.noreply.github.com>
2019-11-04 18:50:01 +0530

    first commit

---
+++ src/app/home/expense.ts
@@ -0,0 +1,8 @@
+export interface Expense{
+    date:Date,
+    name:string,
+    currency:string,
+    amount:number,
+    convertedAmount:number,
+    nativeCurrency:string
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Expense } from './expense';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  expenseList: Expense[] = [];
+  totalExpense:number=0;
+  convertedTotalExpense:number=0;
+
+  addExpense(frm){
+  let expense:Expense;
+   expense = frm.value;
+   let selectedCurrency= expense.nativeCurrency;
+        switch(selectedCurrency){
+          case'INR(70)':{
+            expense.convertedAmount= 70*expense.amount
+            break;
+          }
+          case'INR(60)':{
+            expense.convertedAmount= 60*expense.amount
+            break;
+          }
+          case'INR(50)':{
+            expense.convertedAmount= 50*expense.amount
+            break;
+          }
+          default:{
+              break;
+          }
+        }
+        
+    this.expenseList.push(expense);
+    this.totalExpense = this.totalExpense + expense.amount;
+    this.convertedTotalExpense = this.convertedTotalExpense + expense.convertedAmount;
+   
+   frm.reset()
+  }
+ 
+  constructor() { }
+
+  ngOnInit() { 
+  }
 
 }
+  
